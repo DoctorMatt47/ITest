c2b14439d485d35e711ee56d894309db8f9e7eaa
DoctorMatt <DoctorMatt47@gmail.com>
2021-07-15 23:58:29 +0300

    ClientApp/search - Added search component

---
+++ ITest/ClientApp/src/search/search.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'search',
+    templateUrl: './search.component.html',
+    styleUrls: []
+})
+export class SearchComponent {
+}
