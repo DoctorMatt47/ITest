084ade669a699b3b4647db783010298ea331ac6d
DoctorMatt <DoctorMatt47@gmail.com>
2021-07-15 23:52:06 +0300

    ClientApp/creator - Added creator component

---
+++ ITest/ClientApp/src/creator/creator.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'creator',
+    templateUrl: './creator.component.html',
+    styleUrls: []
+})
+export class CreatorComponent {
+}
