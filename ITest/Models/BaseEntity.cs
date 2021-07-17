f5a5ee6cf498f4db44cd3938b9d6e8d54a40837f
DoctorMatt <DoctorMatt47@gmail.com>
2021-07-17 20:28:41 +0300

    ServerApp/Models - Added base entity

---
+++ ITest/Models/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ITest.Models
+{
+    public abstract class BaseEntity
+    {
+        public virtual Guid Id { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public DateTime? ModifiedDateTime { get; set; }
+    }
+}
