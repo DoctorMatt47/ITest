b9a834b15805bfeb9297787c38728baecda10f68
DoctorMatt <DoctorMatt47@gmail.com>
2021-07-20 19:40:14 +0300

    ServerApp - Added database context

---
+++ ITest/Data/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using ITest.Models.Tests;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITest.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<AnswerChoice> AnswerChoices { get; set; }
+        public DbSet<UserTestAnswer> TestAnswers { get; set; }
+        public DbSet<UserQuestionAnswer> QuestionAnswers { get; set; }
+
+        public DatabaseContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=ITest.db;Cache=Shared");
+        }
+    }
+}
