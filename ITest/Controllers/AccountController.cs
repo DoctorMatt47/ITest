d6b4f8b62cdffaec524aabac08facf7ddcfe1cfe
DoctorMatt <DoctorMatt47@gmail.com>
2021-07-23 17:58:27 +0300

    ServerApp - Changed login method from sync to async.

---
+++ ITest/Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+﻿using ITest.Configs;
+using ITest.Data;
+using ITest.Models.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ITest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly DatabaseContext _db;
+
+        [HttpPost]
+        public async Task<IActionResult> Login(User p)
+        {
+            var identity = await GetIdentity(p.Login, p.Password);
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid username or password." });
+            }
+
+            var now = DateTime.UtcNow;
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                issuer: AuthOptions.ISSUER,
+                audience: AuthOptions.AUDIENCE,
+                notBefore: now,
+                claims: identity.Claims,
+                expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(),
+                SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+
+            return Json(response);
+        }
+
+        private async Task<ClaimsIdentity> GetIdentity(string username, string password)
+        {
+            var person = await _db.Users.FirstOrDefaultAsync(x => x.Login == username && x.Password == password);
+            if (person == null) return null;
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, person.Login)
+            };
+            var claimsIdentity = new
+                ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+            return claimsIdentity;
+        }
+    }
+}
