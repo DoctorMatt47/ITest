435bb41adaeaa8e011b05ba2fc66390993131053
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-01 02:38:36 +0300

    ServerApp/Cqrs - Added add test command and handler

---
+++ ITest/Cqrs/Tests/AddTestCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ITest.Data.Entities.Accounts;
+using ITest.Data.Entities.Tests;
+using MediatR;
+
+namespace ITest.Cqrs.Tests
+{
+    public class AddTestCommand : IRequest<Test>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Account Account { get; set; }
+    }
+}
---
+++ ITest/Cqrs/Tests/AddTestHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ITest.Data;
+using ITest.Data.Entities.Tests;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITest.Cqrs.Tests
+{
+    public class AddTestHandler : BaseHandler, IRequestHandler<AddTestCommand, Test>
+    {
+        public AddTestHandler(DatabaseContext db) : base(db)
+        {
+        }
+
+        public async Task<Test> Handle(AddTestCommand command, CancellationToken cancellationToken)
+        {
+            var newTest = new Test
+            {
+                Title = command.Title,
+                Description = command.Description,
+                Account = command.Account
+            };
+            await _db.Tests.AddAsync(newTest, cancellationToken);
+            await _db.SaveChangesAsync(cancellationToken);
+            return newTest;
+        }
+    }
+}
