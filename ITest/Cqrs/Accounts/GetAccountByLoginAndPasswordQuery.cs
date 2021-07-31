4d017c49a649d8a9cdf4f9b2ad7b73eac8e01393
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-01 02:40:21 +0300

    ServerApp/Exceptions - Added get account by login and password query and handler

---
+++ ITest/Cqrs/Accounts/GetAccountByLoginAndPasswordQuery.cs
@@ -0,0 +1,11 @@
+﻿using ITest.Data.Entities.Accounts;
+using MediatR;
+
+namespace ITest.Cqrs.Accounts
+{
+    public class GetAccountByLoginAndPasswordQuery : IRequest<Account>
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ITest/Cqrs/Accounts/GetAccountByLoginAndPasswordQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ITest.Data;
+using ITest.Data.Entities.Accounts;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITest.Cqrs.Accounts
+{
+    public class GetAccountByLoginAndPasswordQueryHandler : BaseHandler,
+        IRequestHandler<GetAccountByLoginAndPasswordQuery, Account>
+    {
+        public GetAccountByLoginAndPasswordQueryHandler(DatabaseContext db) : base(db)
+        {
+        }
+
+        public async Task<Account> Handle(GetAccountByLoginAndPasswordQuery query, CancellationToken cancellationToken)
+        {
+            var accountToGet = await _db.Accounts.FirstOrDefaultAsync(
+                acc => 
+                (acc.Login == query.Login || acc.Mail == query.Login) &&
+                acc.Password == query.Password,
+                cancellationToken: cancellationToken);
+
+            return accountToGet;
+        }
+    }
+}
