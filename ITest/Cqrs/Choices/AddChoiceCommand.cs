d4d818e7f8e266d94352756f8cc2fd5c5175d017
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-01 02:39:07 +0300

    ServerApp/Cqrs - Added add choice command and handler

---
+++ ITest/Cqrs/Choices/AddChoiceCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ITest.Data.Entities.Tests;
+using MediatR;
+
+namespace ITest.Cqrs.Choices
+{
+    public class AddChoiceCommand : IRequest<Choice>
+    {
+        public string ChoiceString { get; set; }
+        
+        public Question Question { get; set; }
+    }
+}
---
+++ ITest/Cqrs/Choices/AddChoiceHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ITest.Data;
+using ITest.Data.Entities.Tests;
+using MediatR;
+
+namespace ITest.Cqrs.Choices
+{
+    public class AddChoiceHandler : BaseHandler, IRequestHandler<AddChoiceCommand, Choice>
+    {
+        public AddChoiceHandler(DatabaseContext db) : base(db)
+        {
+        }
+
+        public async Task<Choice> Handle(AddChoiceCommand command, CancellationToken cancellationToken)
+        {
+            var newChoice = new Choice
+            {
+                ChoiceString = command.ChoiceString,
+                Question = command.Question,
+            };
+            await _db.Choices.AddAsync(newChoice, cancellationToken);
+            await _db.SaveChangesAsync(cancellationToken);
+            return newChoice;
+        }
+    }
+}
