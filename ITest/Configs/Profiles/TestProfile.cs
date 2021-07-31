652a2b5f25412dd89eba30a048bc7e8e82239065
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-01 02:35:17 +0300

    ServerApp/Configs - Moved test profile from '/Maps' to '/Profiles'

---
+++ ITest/Configs/Profiles/TestProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ITest.Cqrs.Choices;
+using ITest.Cqrs.Questions;
+using ITest.Cqrs.Tests;
+using ITest.Data.Dtos.Tests;
+
+namespace ITest.Configs.Profiles
+{
+    public class TestProfile : Profile
+    {
+        public TestProfile() {
+            CreateMap<TestDto, AddTestCommand>();
+            CreateMap<QuestionDto, AddQuestionCommand>();
+            CreateMap<ChoiceDto, AddChoiceCommand>();
+        }
+    }
+}
