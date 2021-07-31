ea76f0f5ae4b8ddac06bde5949f678a8f81b806c
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-01 02:38:47 +0300

    ServerApp/Cqrs - Added add question command and handler

---
+++ ITest/Cqrs/Questions/AddQuestionCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ITest.Data.Entities.Tests;
+using MediatR;
+
+namespace ITest.Cqrs.Questions
+{
+    public class AddQuestionCommand : IRequest<Question>
+    {
+        public string QuestionString { get; set; }
+        public QuestionType QuestionType { get; set; }
+        public Test Test { get; set; }
+    }
+}
