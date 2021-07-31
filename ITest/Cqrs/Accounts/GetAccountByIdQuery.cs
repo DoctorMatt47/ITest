3b3f06007a48856dc8a89a9cc268a67a8782a27f
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-01 02:40:02 +0300

    ServerApp/Exceptions - Added get account by id query and handler

---
+++ ITest/Cqrs/Accounts/GetAccountByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ITest.Data.Entities.Accounts;
+using MediatR;
+
+namespace ITest.Cqrs.Accounts
+{
+    public class GetAccountByIdQuery : IRequest<Account>
+    {
+        public Guid AccountId { get; set; }
+    }
+}
---
+++ ITest/Cqrs/Accounts/GetAccountByIdQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ITest.Data;
+using ITest.Data.Entities.Accounts;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITest.Cqrs.Accounts
+{
+    public class GetAccountByIdQueryHandler : BaseHandler, IRequestHandler<GetAccountByIdQuery, Account>
+    {
+        public GetAccountByIdQueryHandler(DatabaseContext db) : base(db)
+        {
+        }
+
+        public async Task<Account> Handle(GetAccountByIdQuery query, CancellationToken cancellationToken)
+        {
+            var accountToGet = await _db.Accounts.FirstOrDefaultAsync(
+                acc => acc.Id == query.AccountId,
+                cancellationToken: cancellationToken);
+            return accountToGet;
+        }
+    }
+}
