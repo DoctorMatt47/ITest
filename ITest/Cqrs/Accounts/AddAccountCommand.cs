5e16cd5086d79478f8c9040d5901f3e4945b6844
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-01 02:39:24 +0300

    ServerApp/Cqrs - Added add account command and handler

---
+++ ITest/Cqrs/Accounts/AddAccountCommand.cs
@@ -0,0 +1,13 @@
+﻿using ITest.Data.Entities.Accounts;
+using MediatR;
+
+namespace ITest.Cqrs.Accounts
+{
+    public class AddAccountCommand : IRequest<Account>
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Mail { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ ITest/Cqrs/Accounts/AddAccountHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ITest.Data;
+using ITest.Data.Entities.Accounts;
+using ITest.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITest.Cqrs.Accounts
+{
+    public class AddAccountHandler : BaseHandler, IRequestHandler<AddAccountCommand, Account>
+    {
+        public AddAccountHandler(DatabaseContext db) : base(db)
+        {
+        }
+
+        public async Task<Account> Handle(AddAccountCommand command, CancellationToken cancellationToken)
+        {
+            var accWithSameLoginOrMail = await _db.Accounts.FirstOrDefaultAsync(acc => 
+                acc.Login == command.Login || acc.Mail == command.Mail,
+                cancellationToken: cancellationToken);
+            if (accWithSameLoginOrMail is not null)
+            {
+                if (accWithSameLoginOrMail.Login == command.Login)
+                {
+                    throw new AccountException("An account with this login already exists");
+                }
+                if (accWithSameLoginOrMail.Mail == command.Mail)
+                {
+                    throw new AccountException("An account with this mail already exists");
+                }
+            }
+            var newAccount = new Account
+            {
+                Login = command.Login,
+                Password = command.Password,
+                Mail = command.Mail,
+                City = command.City
+            };
+            await _db.Accounts.AddAsync(newAccount, cancellationToken);
+            await _db.SaveChangesAsync(cancellationToken);
+            return newAccount;
+        }
+    }
+}
