ccc3cd99e90d176af8502835e1ae011aab01cba4
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-01 02:38:04 +0300

    ServerApp/Cqrs - Added base handler

---
+++ ITest/Cqrs/BaseHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ITest.Data;
+
+namespace ITest.Cqrs
+{
+    public abstract class BaseHandler
+    {
+        protected readonly DatabaseContext _db;
+
+        protected BaseHandler(DatabaseContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+    }
+}
