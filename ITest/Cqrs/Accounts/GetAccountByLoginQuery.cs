08852369a4df773b955913a6e4fa48995927ef3c
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-01 17:25:46 +0300

    ServerApp/Cqrs - Added get account by id query and handler

---
+++ ITest/Cqrs/Accounts/GetAccountByLoginQuery.cs
@@ -0,0 +1,10 @@
+﻿using ITest.Data.Entities.Accounts;
+using MediatR;
+
+namespace ITest.Cqrs.Accounts
+{
+    public class GetAccountByLoginQuery : IRequest<Account>
+    {
+        public string Login { get; set; }
+    }
+}
---
+++ ITest/Cqrs/Accounts/GetAccountByLoginQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ITest.Data;
+using ITest.Data.Entities;
+using ITest.Data.Entities.Accounts;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITest.Cqrs.Accounts
+{
+    public class GetAccountByLoginQueryHandler : BaseHandler,
+        IRequestHandler<GetAccountByLoginQuery, Account>
+    {
+        public GetAccountByLoginQueryHandler(DatabaseContext db) : base(db)
+        {
+        }
+        
+        public Task<Account> Handle(GetAccountByLoginQuery query, CancellationToken cancellationToken)
+        {
+            var accountToGet = _db.Accounts.FirstOrDefaultAsync(
+                acc => acc.Login == query.Login,
+                cancellationToken: cancellationToken);
+            return accountToGet;
+        }
+    }
+}
