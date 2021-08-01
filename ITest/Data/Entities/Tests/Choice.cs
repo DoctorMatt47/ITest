6f4724cf733dd0886c311a6c30e400b18ef8de05
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-01 03:14:32 +0300

    ServerApp/Data - Fixed merge issues

--- ITest/Data/Entities/Tests/Choice.cs
+++ ITest/Data/Entities/Tests/Choice.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
 
-namespace ITest.Models.Tests
+namespace ITest.Data.Entities.Tests
 {
     public class Choice : BaseEntity
     {
-        [StringLength(100, MinimumLength=1)]
-        public string? ChoiceString { get; set; }
+        [StringLength(100)]
+        public string ChoiceString { get; set; }
 
         public Guid QuestionId { get; set; }
         public Question Question { get; set; } = default!;
--- ITest/Data/Entities/Tests/TestAnswer.cs
+++ ITest/Data/Entities/Tests/TestAnswer.cs
@@ -1,25 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using ITest.Models.Accounts;
+using ITest.Data.Entities.Accounts;
 
-namespace ITest.Models.Tests
+namespace ITest.Data.Entities.Tests
 {
     public class TestAnswer : BaseEntity
     {
-        [StringLength(100, MinimumLength=1)]
-        public string? Answer { get; set; }
+        [StringLength(100)]
+        public string Answer { get; set; }
             
         public Guid? ChoiceId { get; set; }
-        public Choice? Choice { get; set; }
+        public Choice Choice { get; set; }
 
         public Guid QuestionId { get; set; }
-        public Question Question { get; set; } = null!;
+        public Question Question { get; set; }
 
         public Guid TestId { get; set; }
-        public Test Test { get; set; } = null!;
+        public Test Test { get; set; }
         
         public Guid AccountId { get; set; }
-        public Account Account { get; set; } = null!;
+        public Account Account { get; set; }
     }
 }
