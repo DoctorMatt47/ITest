2f68bb43f7c07a944f48c8907fa9157ca62a6eb4
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-01 17:26:16 +0300

    ServerApp/Cqrs - Added add test and questions and choices command and handler

---
+++ ITest/Cqrs/Tests/AddTestQuestionsChoicesCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ITest.Data.Dtos.Tests;
+using ITest.Data.Entities.Tests;
+using MediatR;
+
+namespace ITest.Cqrs.Tests
+{
+    public class AddTestQuestionsChoicesCommand : IRequest<Test>
+    {
+        public TestDto TestDto { get; set; }
+        public Guid AccountId { get; set; }
+    }
+}
