199aab098397054c8e9dfe386c170c976fd18da5
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-02 20:59:05 +0300

    ServerApp/Cqrs - Added get question by id query and handler

---
+++ ITest/Cqrs/Questions/GetQuestionsByTestIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using ITest.Data.Entities.Tests;
+using MediatR;
+
+namespace ITest.Cqrs.Questions
+{
+    public class GetQuestionsByTestIdQuery : IRequest<ICollection<Question>>
+    {
+        public GetQuestionsByTestIdQuery(Guid testId) => TestId = testId;
+        
+        public Guid TestId { get; }
+    }
+}
---
+++ ITest/Cqrs/Questions/GetQuestionsByTestIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ITest.Data;
+using ITest.Data.Entities.Tests;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITest.Cqrs.Questions
+{
+    public class GetQuestionsByTestIdQueryHandler : BaseHandler,
+        IRequestHandler<GetQuestionsByTestIdQuery, ICollection<Question>>
+    {
+        public GetQuestionsByTestIdQueryHandler(DatabaseContext db) : base(db)
+        {
+        }
+
+        public async Task<ICollection<Question>> Handle(GetQuestionsByTestIdQuery query,
+            CancellationToken cancellationToken) => await _db.Questions
+            .Where(question => question.TestId == query.TestId).ToListAsync(cancellationToken);
+    }
+}
