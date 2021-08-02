4c7c759a37161ce814f1aedb41ad211000f90543
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-02 20:58:42 +0300

    ServerApp/Cqrs - Added get test by id query and handler

---
+++ ITest/Cqrs/Tests/GetTestByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ITest.Data.Entities.Tests;
+using MediatR;
+
+namespace ITest.Cqrs.Tests
+{
+    public class GetTestByIdQuery : IRequest<Test>
+    {
+        public GetTestByIdQuery(Guid testId) => TestId = testId;
+        
+        public Guid TestId { get; }
+    }
+}
