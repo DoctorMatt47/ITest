db0da34c91a16e459c48900a16f00627dacaf895
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-02 21:22:51 +0300

    ServerApp/Profiles Add maps for test

--- ITest/Configs/Profiles/TestProfile.cs
+++ ITest/Configs/Profiles/TestProfile.cs
@@ -3,6 +3,7 @@ using ITest.Cqrs.Choices;
 using ITest.Cqrs.Questions;
 using ITest.Cqrs.Tests;
 using ITest.Data.Dtos.Tests;
+using ITest.Data.Entities.Tests;
 
 namespace ITest.Configs.Profiles
 {
@@ -12,6 +13,13 @@ namespace ITest.Configs.Profiles
             CreateMap<TestDto, AddTestCommand>();
             CreateMap<QuestionDto, AddQuestionCommand>();
             CreateMap<ChoiceDto, AddChoiceCommand>();
+            CreateMap<TestDto, AddTestQuestionsChoicesCommand>();
+            CreateMap<TestDto, Test>();
+            CreateMap<QuestionDto, Question>();
+            CreateMap<ChoiceDto, Choice>();
+            CreateMap<Test, TestDto>();
+            CreateMap<Question, QuestionDto>();
+            CreateMap<Choice, ChoiceDto>();
         }
     }
 }
