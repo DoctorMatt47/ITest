d3eebbb652e2a1a087df14ff55fdfaa115767040
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-02 20:59:29 +0300

    ServerApp/Cqrs - Added get choices by question id query and handler

---
+++ ITest/Cqrs/Choices/GetChoicesByQuestionIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using ITest.Data.Entities.Tests;
+using MediatR;
+
+namespace ITest.Cqrs.Choices
+{
+    public class GetChoicesByQuestionIdQuery : IRequest<ICollection<Choice>>
+    {
+        public GetChoicesByQuestionIdQuery(Guid questionId) => QuestionId = questionId;
+
+        public Guid QuestionId { get; }
+    }
+}
---
+++ ITest/Cqrs/Choices/GetChoicesByQuestionIdQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ITest.Data;
+using ITest.Data.Entities.Tests;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITest.Cqrs.Choices
+{
+    public class GetChoicesByQuestionIdQueryHandler : BaseHandler,
+        IRequestHandler<GetChoicesByQuestionIdQuery, ICollection<Choice>>
+    {
+        public GetChoicesByQuestionIdQueryHandler(DatabaseContext db) : base(db)
+        {
+        }
+
+        public async Task<ICollection<Choice>> Handle(GetChoicesByQuestionIdQuery query,
+            CancellationToken cancellationToken) => await _db.Choices
+            .Where(choice => choice.QuestionId == query.QuestionId)
+            .ToListAsync(cancellationToken);
+    }
+}
