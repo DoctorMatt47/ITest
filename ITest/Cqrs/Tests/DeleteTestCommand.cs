1a5ef59de0f0ec8e6d33b25296cb73ea93d48824
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-02 21:23:30 +0300

    ServerApp/Cqrs - Added delete test command and handler

---
+++ ITest/Cqrs/Tests/DeleteTestCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MediatR;
+
+namespace ITest.Cqrs.Tests
+{
+    public class DeleteTestCommand : IRequest<Unit>
+    {
+        public DeleteTestCommand(Guid testId, Guid accountId)
+        {
+            TestId = testId;
+            AccountId = accountId;
+        }
+
+        public Guid TestId { get; }
+        public Guid AccountId { get; }
+    }
+}
---
+++ ITest/Cqrs/Tests/DeleteTestCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ITest.Data;
+using ITest.Exceptions.Tests;
+using MediatR;
+
+namespace ITest.Cqrs.Tests
+{
+    public class DeleteTestCommandHandler : BaseHandler, IRequestHandler<DeleteTestCommand>
+    {
+        private readonly IMediator _mediator;
+
+        public DeleteTestCommandHandler(DatabaseContext db, IMediator mediator) : base(db)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<Unit> Handle(DeleteTestCommand command, CancellationToken cancellationToken)
+        {
+            var testToDelete = 
+                await _mediator.Send(new GetTestByIdQuery(command.TestId), cancellationToken);
+            if (testToDelete is null)
+            {
+                throw new TestNotFoundException("Test with passed id does not exist");
+            }
+            if (testToDelete.AccountId != command.AccountId)
+            {
+                throw new TestForbiddenException("Test with passed id does not belong to your account");
+            }
+
+            _db.Remove(testToDelete);
+            await _db.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
