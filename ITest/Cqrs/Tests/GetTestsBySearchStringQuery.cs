2524f44afcc8c7910a5f19e908b42c04597bebc8
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-08 17:40:59 +0300

    ServerApp/Cqrs - Added get tests by search string query and handler

---
+++ ITest/Cqrs/Tests/GetTestsBySearchStringQuery.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ITest.Data.Entities.Tests;
+using MediatR;
+
+namespace ITest.Cqrs.Tests
+{
+    public class GetTestsBySearchStringQuery : IRequest<ICollection<Test>>
+    {
+        public GetTestsBySearchStringQuery(string searchString) => SearchString = searchString;
+        
+        public string SearchString { get; set; }
+        public int PagesToSkip { get; set; }
+    }
+}
