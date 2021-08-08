e65476c78ce9e421041381e30a46b6f56b653eab
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-08 17:40:34 +0300

    ServerApp/Controllers - Added test search controller

---
+++ ITest/Controllers/SearchTestController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ITest.Cqrs.Tests;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITest.Controllers
+{
+    [ApiController]
+    [Route("api/search")]
+    public class SearchTestController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public SearchTestController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("{query.PagesToSkip}/{query.SearchString}")]
+        public async Task<IEnumerable> GetBySearchString(GetTestsBySearchStringQuery query,
+            CancellationToken cancellationToken)
+        {
+            var searchedTests = await _mediator.Send(query, cancellationToken);
+            return searchedTests;
+        }
+    }
+}
