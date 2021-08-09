8869bb6d50ab5d036ce54658d4783a83e44437b5
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-09 22:56:04 +0300

    ServerApp/Cqrs - Added get account by login or mail query and handler

---
+++ ITest/Cqrs/Accounts/GetAccountByLoginOrMailQuery.cs
@@ -0,0 +1,18 @@
+﻿using ITest.Data.Entities.Accounts;
+using MediatR;
+
+namespace ITest.Cqrs.Accounts
+{
+    public class GetAccountByLoginOrMailQuery : IRequest<Account>
+    {
+        public GetAccountByLoginOrMailQuery(string login, string mail)
+        {
+            Login = login;
+            Mail = mail;
+        }
+        
+        public string Login { get; set; }
+        
+        public string Mail { get; set; }
+    }
+}
