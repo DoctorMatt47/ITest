dfbb46eda8d1cdc82d83874a859e1e1b08df96b2
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-14 00:45:52 +0300

    ServerApp/Cqrs - Added get test count by search count query and handler

---
+++ ITest/Cqrs/Tests/GetTestCountBySearchStringQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace ITest.Cqrs.Tests
+{
+    public class GetTestCountBySearchStringQuery : IRequest<int>
+    {
+        public GetTestCountBySearchStringQuery(string searchString) => SearchString = searchString;
+        
+        public string SearchString { get; set; }
+    }
+}
