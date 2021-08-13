8ee4f69044fddba87882e9fec3ff57f4a3ab9ca4
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-14 01:04:34 +0300

    ServerApp/Configs - Added test answer map profile

---
+++ ITest/Configs/Profiles/TestAnswerProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ITest.Data.Dtos.TestAnswers;
+using ITest.Data.Entities.Tests;
+
+namespace ITest.Configs.Profiles
+{
+    public class TestAnswerProfile : Profile
+    {
+        public TestAnswerProfile()
+        {
+            CreateMap<TestAnswerDto, TestAnswer>();
+        }
+        
+    }
+}
