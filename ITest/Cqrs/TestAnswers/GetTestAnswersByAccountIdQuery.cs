0561dab6ea19caa16d9d8639bb431e5e08c3dfa4
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-14 01:04:59 +0300

    ServerApp/Cqrs - Added get test answers by account id query and handler

---
+++ ITest/Cqrs/TestAnswers/GetTestAnswersByAccountIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using ITest.Data.Entities.Tests;
+using MediatR;
+
+namespace ITest.Cqrs.TestAnswers
+{
+    public class GetTestAnswersByAccountIdQuery : IRequest<IEnumerable<TestAnswer>>
+    {
+        public GetTestAnswersByAccountIdQuery(Guid accountId) => AccountId = accountId;
+
+        public Guid AccountId { get; set; }
+    }
+}
