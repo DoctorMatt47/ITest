1d524078d3ba7ce87df2bc557742f4b3d95c67e2
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-14 01:04:03 +0300

    ServerApp/Data - Added test answer dto

---
+++ ITest/Data/Dtos/TestAnswers/TestAnswerDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ITest.Data.Dtos.TestAnswers
+{
+    public class TestAnswerDto
+    {
+        public string Answer { get; set; }
+            
+        public Guid? ChoiceId { get; set; }
+
+        public Guid QuestionId { get; set; }
+    }
+}
