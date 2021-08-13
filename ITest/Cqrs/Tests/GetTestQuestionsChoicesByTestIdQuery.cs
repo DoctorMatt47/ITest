0b7cecbcbc9843e5006fc1091d1b6d26049e34a7
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-14 01:02:30 +0300

    ServerApp/Cqrs - Added get test, questions and choices by test id query and handler

---
+++ ITest/Cqrs/Tests/GetTestQuestionsChoicesByTestIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ITest.Data.Entities.Tests;
+using MediatR;
+
+namespace ITest.Cqrs.Tests
+{
+    public class GetTestQuestionsChoicesByTestIdQuery : IRequest<Test>
+    {
+        public GetTestQuestionsChoicesByTestIdQuery(Guid testId) => TestId = testId;
+        
+        public Guid TestId { get; set; }
+    }
+}
---
+++ ITest/Cqrs/Tests/GetTestQuestionsChoicesByTestIdQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper.Internal;
+using ITest.Cqrs.Choices;
+using ITest.Cqrs.Questions;
+using ITest.Data;
+using ITest.Data.Entities.Tests;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITest.Cqrs.Tests
+{
+    public class GetTestQuestionsChoicesByTestIdQueryHandler : BaseHandler,
+        IRequestHandler<GetTestQuestionsChoicesByTestIdQuery, Test>
+    {
+        private readonly IMediator _mediator;
+
+        public GetTestQuestionsChoicesByTestIdQueryHandler(DatabaseContext db, IMediator mediator) : base(db)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<Test> Handle(GetTestQuestionsChoicesByTestIdQuery query,
+            CancellationToken cancellationToken)
+        {
+            var testToGet = 
+                await _mediator.Send(new GetTestByIdQuery(query.TestId), cancellationToken);
+
+            if (testToGet == null) return null;
+
+            var questionsToGet =
+                await _mediator.Send(new GetQuestionsByTestIdQuery(query.TestId), cancellationToken);
+            
+            var choicesToGetNestedTasks = questionsToGet.Select(async q =>
+                await _mediator.Send(new GetChoicesByQuestionIdQuery(q.Id), cancellationToken));
+            
+            var choicesToGet = await Task.WhenAll(choicesToGetNestedTasks);
+
+            return testToGet;
+        }
+    }
+}
