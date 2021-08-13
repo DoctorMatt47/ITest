e390f190109262042f1951eec388855460acaec4
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-14 01:01:35 +0300

    ServerApp/Controller - Added get pages count method to the controller

--- ITest/Controllers/SearchTestController.cs
+++ ITest/Controllers/SearchTestController.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using ITest.Cqrs.Tests;
+using ITest.Data.Entities.Tests;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +14,7 @@ namespace ITest.Controllers
     [Route("api/search")]
     public class SearchTestController : Controller
     {
+        private const int MaxElementsOnOnePage = 20;
         private readonly IMediator _mediator;
 
         public SearchTestController(IMediator mediator)
@@ -21,11 +24,19 @@ namespace ITest.Controllers
 
         [HttpGet]
         [Route("{query.PagesToSkip}/{query.SearchString}")]
-        public async Task<IEnumerable> GetBySearchString(GetTestsBySearchStringQuery query,
+        public async Task<IEnumerable<Test>> GetBySearchString(GetTestsBySearchStringQuery query,
             CancellationToken cancellationToken)
         {
             var searchedTests = await _mediator.Send(query, cancellationToken);
             return searchedTests;
+        }
+
+        public async Task<int> GetPagesCount(GetTestCountBySearchStringQuery query,
+            CancellationToken cancellationToken)
+        {
+            var testCount = await _mediator.Send(query, cancellationToken);
+            var a = (double) testCount / MaxElementsOnOnePage;
+            return (int) Math.Ceiling(a);
         }
     }
 }
