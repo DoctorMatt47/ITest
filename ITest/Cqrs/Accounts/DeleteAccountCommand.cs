9348fad822995523e48e00f52f05613b977abe4e
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-16 22:40:16 +0300

    ServerApp/Cqrs - Added delete account command and handler

---
+++ ITest/Cqrs/Accounts/DeleteAccountCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MediatR;
+
+namespace ITest.Cqrs.Accounts
+{
+    public class DeleteAccountCommand : IRequest
+    {
+        public Guid AccountId { get; set; }
+        public string Password { get; set; }
+    }
+}
