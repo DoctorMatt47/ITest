454d19019dd93f438b25e9299ef75cb49c5abdf7
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-18 19:18:37 +0300

    ClientApp/signup - Implemented signup component

--- ITest/ClientApp/src/signup/signup.component.ts
+++ ITest/ClientApp/src/signup/signup.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AccountService} from "../services/api/account.service";
+import {Account} from "../models/accounts/account";
+import {Observer} from "rxjs";
+import {HttpErrorResponse} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'signup',
     templateUrl: './signup.component.html',
-    styleUrls: []
+    styleUrls: [],
+    providers: [AccountService]
 })
 export class SignUpComponent {
+    account: Account = new Account();
+    confirmPassword: string = "";
+    loginPath: string = "/login"
+
+    constructor(
+        private _accounts: AccountService,
+        private _router: Router) {
+    }
+
+    signup(): void {
+        if (this.confirmPassword != this.account.password) {
+            return;
+        }
+        let observer: Observer<Account> = {
+            error: (response: HttpErrorResponse) => {
+                console.log(response);
+            },
+            next: (data: Account) => {
+                console.log(data);
+            },
+            complete: () => {
+                this._router.navigate([this.loginPath]);
+            }
+        }
+        this._accounts.register(this.account).subscribe(observer)
+    }
 }
