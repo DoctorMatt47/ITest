09011937c677cb9f5c50b69301a39a0b2111fa50
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-18 17:26:23 +0300

    ClientApp/services - Added account service

---
+++ ITest/ClientApp/src/services/api/account.service.ts
@@ -0,0 +1,41 @@
+﻿import {Injectable} from "@angular/core";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Account} from "../../models/accounts/account";``
+
+@Injectable()
+export class AccountService {
+    private readonly _url = "/api/accounts";
+
+    constructor(private _http: HttpClient) {
+    }
+
+    login(loginOrEmail: string, password: string) {
+        let body = {
+            login: loginOrEmail,
+            password: password
+        }
+        return this._http.post(this._url + '/login', body);
+    }
+
+    register(acc: Account) {
+        let body = {
+            login: acc.login,
+            password: acc.password,
+            mail: acc.mail,
+            city: acc.city
+        }
+        return this._http.post(this._url + '/register', body);
+    }
+    
+    delete(password: string) {
+        let options = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+            }),
+            body: {
+                password: password
+            },
+        };
+        return this._http.delete(this._url + 'delete', options);
+    }
+}
