38d9e51f4eb1776ff72027433c3c36b1a007ad19
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-18 17:26:31 +0300

    ClientApp/services - Added token service

---
+++ ITest/ClientApp/src/services/tokens/token.service.ts
@@ -0,0 +1,18 @@
+﻿import {Injectable} from "@angular/core";
+import {CookieService} from "ngx-cookie-service";
+
+@Injectable()
+export class TokenService {
+    constructor(
+        private _cookie: CookieService
+    ) {
+    }
+
+    get jwtToken(): string {
+        return this._cookie.get("ITest_jwtToken");
+    }
+
+    set jwtToken(value: string) {
+        this._cookie.set("ITest_jwtToken", value);
+    }
+}
