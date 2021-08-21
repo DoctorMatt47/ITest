73df9a8af4d2ea41011367204e5ff2cf4a10e787
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-21 22:06:50 +0300

    ClientApp - Created test-preview component

---
+++ ITest/ClientApp/src/test-preview/test-preview.component.ts
@@ -0,0 +1,12 @@
+﻿import {Component} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+    selector: 'test-preview',
+    templateUrl: './test-preview.component.html',
+    styleUrls: [],
+    providers: [HttpClient]
+})
+export class TestPreviewComponent {
+    
+}
