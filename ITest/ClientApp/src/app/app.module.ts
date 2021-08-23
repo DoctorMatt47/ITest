6ab792861020e5f9cec09e07be3c37141f85b9b4
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-23 21:19:39 +0300

    ClientApp/test-preview - Added test preview component

--- ITest/ClientApp/src/app/app.module.ts
+++ ITest/ClientApp/src/app/app.module.ts
@@ -3,6 +3,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {Router, RouterModule, Routes} from '@angular/router';
 import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {CookieService} from 'ngx-cookie-service';
 
 import {SearchComponent} from '../search/search.component';
 import {CreatorComponent} from '../creator/creator.component';
@@ -10,14 +12,14 @@ import {TestComponent} from '../test/test.component';
 import {LoginComponent} from '../login/login.component';
 import {SignUpComponent} from '../signup/signup.component';
 import {NotFoundComponent} from '../not-found/not-found.component';
-import {HttpClientModule} from "@angular/common/http";
-import {CookieService} from 'ngx-cookie-service';
+import {TestPreviewComponent} from "../test-preview/test-preview.component";
 
 const appRoutes: Routes = [
     {path: '', redirectTo: "search", pathMatch: "full"},
     {path: 'search', component: SearchComponent},
     {path: 'creator', component: CreatorComponent},
-    {path: 'test', component: TestComponent},
+    {path: 'test/:id', component: TestComponent},
+    {path: 'test-preview/:id', component: TestPreviewComponent},
     {path: 'login', component: LoginComponent},
     {path: 'signup', component: SignUpComponent},
     {path: '**', component: NotFoundComponent}
@@ -27,7 +29,7 @@ const appRoutes: Routes = [
     imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
     declarations: [
         AppComponent, SearchComponent, CreatorComponent, TestComponent,
-        LoginComponent, SignUpComponent, NotFoundComponent
+        LoginComponent, SignUpComponent, NotFoundComponent, TestPreviewComponent
     ],
     providers: [CookieService],
     bootstrap: [AppComponent]
