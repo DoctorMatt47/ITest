15c1baacdedb526366785c11bec476f6d6e6417a
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-23 21:23:09 +0300

    ClientApp/services - Added test repository service

---
+++ ITest/ClientApp/src/services/api/test-repository.service.ts
@@ -0,0 +1,35 @@
+﻿import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Test} from "../../models/tests/test.model";
+import {TokenService} from "../tokens/token.service";
+import {Observable} from "rxjs";
+
+@Injectable()
+export class TestRepositoryService {
+    private readonly _url = "/api/tests";
+
+    constructor(private _http: HttpClient,
+                private _token: TokenService) {
+    }
+    
+    getTest(id: string): Observable<any> {
+        const getString = this._url + '/' + id;
+        return this._http.get(getString);
+    }
+    
+    getTestPreview(id: string): Observable<any> {
+        const getString = this._url + '/preview/' + id;
+        return this._http.get(getString);
+    }
+    
+    createTest(test: Test): Observable<any> {
+        const jwtToken = this._token.jwtToken;
+        const options = {
+            headers: new HttpHeaders({
+                Authorization: `bearer ${jwtToken}`
+            }),
+            observe: 'response' as 'body'
+        };
+        return this._http.post(this._url, test, options);
+    }
+}
