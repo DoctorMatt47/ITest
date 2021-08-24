4cd8190c2281034ac43b13af2aa1d33692414b96
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-24 23:33:57 +0300

    ServerApp/Services - Added identity service interface and implementation

---
+++ ITest/Services/Identity/IIdentityService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ITest.Data.Entities.Accounts;
+
+namespace ITest.Services.Identity
+{
+    public interface IIdentityService
+    {
+        ClaimsIdentity GetAccountIdentity(Account userAccount);
+    }
+}
---
+++ ITest/Services/Identity/IdentityService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using ITest.Data.Entities.Accounts;
+
+namespace ITest.Services.Identity
+{
+    public class IdentityService : IIdentityService
+    {
+        public ClaimsIdentity GetAccountIdentity(Account userAccount)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userAccount.Login),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, userAccount.Role.ToString()),
+                new Claim(ClaimTypes.SerialNumber, userAccount.Id.ToString())
+            };
+            return new ClaimsIdentity(
+                claims, "Token", 
+                ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+        }
+    }
+}
