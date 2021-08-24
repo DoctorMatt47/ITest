0abf84e739df352584fa942c48a2e87b87c346ff
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-24 23:36:54 +0300

    ServerApp/Extensions - Added extension for claims principal class

---
+++ ITest/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace ITest.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static Guid GetUserAccountId(this ClaimsPrincipal user)
+        {
+            var userId = user.FindFirst(ClaimTypes.SerialNumber)?.Value;
+            if (!Guid.TryParse(userId, out var userGuid))
+            {
+                throw new ArgumentException(nameof(ClaimTypes.SerialNumber));
+            }
+
+            return userGuid;
+        }
+    }
+}
