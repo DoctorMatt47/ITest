f5f5e02913cddfb071e61ebbd3d581fc08e2c48e
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-26 02:35:28 +0300

    ServerApp/Configs - Returned auth options class

---
+++ ITest/Configs/AuthOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ITest.Configs
+{
+    public static class AuthOptions
+    {
+        public const string Issuer = "ITestServer";
+        public const string Audience = "ITestClient";
+        public const string Key = "71574d2b0054c63f2ffa8319c8ec9e10a69be721e39ee55a6d2c52ab5e93ff45";
+        public const int Lifetime = 1;
+
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AuthOptions.Key));
+        }
+    }
+}
--- ITest/Services/Tokens/TokenService.cs
+++ ITest/Services/Tokens/TokenService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
+using ITest.Configs;
 using ITest.Data.Entities.Accounts;
 using ITest.Services.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.IdentityModel.Tokens;
 
 namespace ITest.Services.Tokens
@@ -11,11 +13,6 @@ namespace ITest.Services.Tokens
     {
         private readonly IIdentityService _identity;
 
-        private const string Issuer = "ITestServer";
-        private const string Audience = "ITestClient";
-        private const string Key = "71574d2b0054c63f2ffa8319c8ec9e10a69be721e39ee55a6d2c52ab5e93ff45";
-        private const int Lifetime = 1; // время жизни токена - 1 минута
-
         public TokenService(IIdentityService identity) => _identity = identity;
 
         public string CreateJwtToken(Account account)
@@ -23,20 +20,17 @@ namespace ITest.Services.Tokens
             var identity = _identity.GetAccountIdentity(account);
 
             var now = DateTime.UtcNow;
-            var jwt = new JwtSecurityToken(Issuer, Audience,
+            var jwt = new JwtSecurityToken(
+                AuthOptions.Issuer,
+                AuthOptions.Audience,
                 notBefore: now,
                 claims: identity.Claims,
-                expires: now.Add(TimeSpan.FromMinutes(Lifetime)),
+                expires: now.Add(TimeSpan.FromMinutes(AuthOptions.Lifetime)),
                 signingCredentials: new SigningCredentials(
-                    GetSymmetricSecurityKey(),
+                    AuthOptions.GetSymmetricSecurityKey(),
                     SecurityAlgorithms.HmacSha256)
             );
             return new JwtSecurityTokenHandler().WriteToken(jwt);
-        }
-
-        private static SymmetricSecurityKey GetSymmetricSecurityKey()
-        {
-            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Key));
         }
     }
 }
