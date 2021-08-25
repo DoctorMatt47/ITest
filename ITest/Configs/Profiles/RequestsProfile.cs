8f1f9fc1ff3e2ce03b4100b30b403852bdef074d
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-26 02:39:00 +0300

    ServerApp/Cqrs - Added request and response profiles

---
+++ ITest/Configs/Profiles/RequestsProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ITest.Cqrs.Accounts;
+using ITest.Cqrs.Tests;
+using ITest.Data.Dtos.Requests.Accounts;
+using ITest.Data.Dtos.Requests.Tests;
+
+namespace ITest.Configs.Profiles
+{
+    public class RequestsProfile : Profile
+    {
+        public RequestsProfile()
+        {
+            CreateMap<LoginAccountRequest, GetAccountByLoginAndPasswordQuery>();
+            CreateMap<RegisterAccountRequest, AddAccountCommand>();
+            CreateMap<DeleteAccountRequest, DeleteAccountCommand>();
+            CreateMap<TestQuestionsChoicesRequest, AddTestQuestionsChoicesCommand>();
+            CreateMap<DeleteAccountRequest, DeleteAccountCommand>();
+            CreateMap<DeleteAccountRequest, DeleteAccountCommand>();
+        }
+    }
+}
---
+++ ITest/Configs/Profiles/ResponsesProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ITest.Cqrs.Accounts;
+using ITest.Data.Dtos.Requests.Accounts;
+using ITest.Data.Dtos.Responses.Tests;
+using ITest.Data.Entities.Tests;
+
+namespace ITest.Configs.Profiles
+{
+    public class ResponsesProfile : Profile
+    {
+        public ResponsesProfile()
+        {
+            CreateMap<Test, TestResponse>();
+            CreateMap<Test, TestQuestionsChoicesResponse>();
+        }
+    }
+}
