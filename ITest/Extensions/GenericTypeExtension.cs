ebabc7879bc2f405a4e65be72139e4599817eb84
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-26 02:39:23 +0300

    ServerApp/Extensions - Added object extension

---
+++ ITest/Extensions/GenericTypeExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ITest.Extensions
+{
+    public static class GenericTypeExtension
+    {
+        private static string GetGenericTypeName(this Type type)
+        {
+            var typeName = string.Empty;
+
+            if (type.IsGenericType)
+            {
+                var genericTypes = 
+                    string.Join(",", type.GetGenericArguments().Select(t => t.Name).ToArray());
+                typeName = $"{type.Name.Remove(type.Name.IndexOf('`'))}<{genericTypes}>";
+            }
+            else
+            {
+                typeName = type.Name;
+            }
+
+            return typeName;
+        }
+
+        public static string GetGenericTypeName(this object @object) 
+            => @object.GetType().GetGenericTypeName();
+    }
+}
