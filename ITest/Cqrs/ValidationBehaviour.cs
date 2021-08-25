a89824b4dd2a04b7729acde5b24034b7a36a3bcc
DoctorMatt <DoctorMatt47@gmail.com>
2021-08-26 02:36:11 +0300

    ServerApp/Cqrs - Added validation pipeline

---
+++ ITest/Cqrs/ValidationBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+using ITest.Exceptions.Cqrs;
+using ITest.Extensions;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ITest.Cqrs
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<ValidationBehaviour<TRequest, TResponse>> _logger;
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators,
+            ILogger<ValidationBehaviour<TRequest, TResponse>> logger)
+        {
+            _validators = validators;
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            if (!_validators.Any()) return await next();
+            
+            var typeName = request.GetGenericTypeName();
+
+            _logger.LogInformation("----- Validating command {CommandType}", typeName);
+
+
+            var context = new ValidationContext<TRequest>(request);
+            var validationResults =
+                await Task.WhenAll(_validators.Select(v =>
+                    v.ValidateAsync(context, cancellationToken)));
+            
+            var failures = validationResults.SelectMany(result => result.Errors)
+                .Where(error => error != null).ToList();
+            
+            if (!failures.Any()) return await next();
+            
+            _logger.LogWarning(
+                "Validation errors - {CommandType} - Command: {@Command} - Errors: {@ValidationErrors}",
+                typeName, request, failures);
+
+            throw new CqrsValidationException(
+                $"Command Validation Errors for type {typeof(TRequest).Name}",
+                new ValidationException("Validation exception", failures));
+        }
+    }
+}
