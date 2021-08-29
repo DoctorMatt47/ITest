038c7e8627eebc7fbd12ab78cacb3ec5751f7eff
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-08-29 19:19:06 +0300

    ClientApp/models - Added base model inheritance

--- ITest/ClientApp/src/models/tests/choices/choice.model.ts
+++ ITest/ClientApp/src/models/tests/choices/choice.model.ts
@@ -1,3 +1,5 @@
-﻿export class Choice {
+﻿import {BaseModel} from "../../base.model";
+
+export class Choice extends BaseModel {
     choiceString?: string;
 }
--- ITest/ClientApp/src/models/tests/question/question.model.ts
+++ ITest/ClientApp/src/models/tests/question/question.model.ts
@@ -1,7 +1,8 @@
 ﻿import {QuestionType} from "./question-type.enum";
 import {Choice} from "../choices/choice.model";
+import {BaseModel} from "../../base.model";
 
-export class Question {
+export class Question extends BaseModel {
     questionString?: string;
     questionType?: QuestionType;
     choices?: Array<Choice>;
