2f3c09dfbe7eea4e68c350988174291295d8954e
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-08-29 19:13:28 +0300

    Project - Added migration

---
+++ ITest/Data/Migrations/20210829095833_ChangeMailOnEmail.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ITest.Migrations
+{
+    public partial class ChangeMailOnEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Mail",
+                table: "Accounts",
+                newName: "Email");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Accounts",
+                newName: "Mail");
+        }
+    }
+}
