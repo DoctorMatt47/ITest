9c6ed28290914c543bfafb9bf64fe310c8b0e4f1
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-08-30 21:16:10 +0300

    ClientApp/services - Added header with token for post request

--- ITest/ClientApp/src/services/api/test-answers-repository.service.ts
+++ ITest/ClientApp/src/services/api/test-answers-repository.service.ts
@@ -1,19 +1,38 @@
 ﻿import {Test} from "../../models/tests/test.model";
 import {ActivatedRoute} from "@angular/router";
-import {TestQuestionsChoicesService} from "./test-questions-choices.service";
+import {TestQuestionsChoicesRepositoryService} from "./test-questions-choices-repository.service";
 import {TestAnswer} from "../../models/test-answers/test-answer";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Injectable} from "@angular/core";
+import {TokenService} from "../tokens/token.service";
 
 @Injectable()
 export class TestAnswersRepositoryService {
     private readonly _url = "api/answers";
     
-    constructor(private _http: HttpClient) {
+    constructor(private _http: HttpClient,
+                private _token: TokenService) {
     }
     
     create(testId: string, answers: Array<TestAnswer>) {
+        const answersList = answers.filter((ans) => {
+            return {
+                answer: ans.answer,
+                choiceId: ans.choiceId,
+                questionId: ans.questionId
+            };
+        });
+        const body = {
+            answers: answersList
+        };
+        const jwtToken = this._token.jwtToken;
+        const options = {
+            headers: new HttpHeaders({
+                Authorization: `bearer ${jwtToken}`
+            }),
+            observe: 'response' as 'body'
+        };
         const createUrl = this._url + '/' + testId;
-        return this._http.post(createUrl, {answers});
+        return this._http.post(createUrl, body, options);
     }
 }
